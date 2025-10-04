9fa54e0abe0c6e6cf71873af5a5aef53ac98ba1d
liliumauratumn <130629903+liliumauratumn@users.noreply.github.com>
2025-10-05 07:26:08 +0900

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,14 @@
+// pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ja">
+      <Head>
+        {/* faviconの指定を追加 */}
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
