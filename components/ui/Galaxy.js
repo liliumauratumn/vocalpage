732d6c135e363079e6eaa1e3c5bc71dd6cbce46b
liliumauratumn <130629903+liliumauratumn@users.noreply.github.com>
2025-10-05 07:47:43 +0900

    Update Galaxy.js

--- components/ui/Galaxy.js
+++ components/ui/Galaxy.js
@@ -334,7 +334,7 @@ export default function Galaxy({
     <div 
       ref={ctnDom} 
       style={{
-    //    position: 'relative',
+    position: 'static',
         width: '100%',
         height: '100%'
       }}
